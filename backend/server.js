91d02b501b8059560702485e18718b0b559490f5
Jase Mwaura <rawscript170@gmail.com>
2025-12-10 13:28:06 -0800

    enh db switch

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use('/api/uploads', uploadRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
 // Connect to MongoDB and start server
-connectDB().then(() => {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-}).catch(err => {
-  console.error("❌ Failed to start server:", err);
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
